c6a55431ad567e33cff0c882c47881802e75956c
pranav bhatane <pranav.bhatane@think360.ai>
2022-09-07 19:32:31 +0530

    added login css

--- react-assignment/src/page/login.js
+++ react-assignment/src/page/login.js
@@ -1,36 +1,45 @@
 import { useState } from "react";
+import './login.css';
 
 export default function Login() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
-  
+
     function handleSubmit(event) {
-      event.preventDefault();
-      console.log('username:', username);
-      console.log('password:', password);
+        event.preventDefault();
+        console.log('username:', username);
+        console.log('password:', password);
     }
-  
+
     return (
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="username">Username</label>
-          <input
-            id="username"
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
+        <div className="fullContent">
+            <span className="Header">Think360</span><form onSubmit={handleSubmit}>
+                <div className="Box">
+                    <div className="content1">
+                        <label htmlFor="username">Username</label>
+                        <br />
+                        <input
+                            id="username"
+                            type="text"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)} />
+                    </div>
+                    <br />
+                    <div className="content2">
+                        <label htmlFor="password">Password</label>
+                        <br />
+                        <input
+                            id="password"
+                            type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)} />
+                    </div>
+                    <br />
+                    <button type="submit">Submit</button>
+                </div>
+            </form>
+
         </div>
-        <div>
-          <label htmlFor="password">Password</label>
-          <input
-            id="password"
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <button type="submit">Submit</button>
-      </form>
+
     );
-  }
+}
